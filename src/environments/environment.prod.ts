0f7c2443acb14ea012054c14cd91907315d36b62
Damien <damien.auversack@outlook.be>
2024-11-10 15:34:12 +0100

    add dev and prod environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://damien-auversack.com', // URL pour la production
+  port: '3000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost', // URL pour le développement,
+  port: '3000'
+};
