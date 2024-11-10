b7a795fb5d24e2d5dee25e2850f4d421fc8a0b4e
Damien <damien.auversack@outlook.be>
2024-11-10 15:36:24 +0100

    update routes file with torrent pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from "./login/login.component";
-import {RegistrationComponent} from "./registration/registration.component";
-import {MainComponent} from "./main/main.component";
-import {AdminComponent} from "./admin/admin.component";
+import { LoginComponent } from "./login/login.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { MainComponent } from "./main/main.component";
+import { AdminComponent } from "./admin/admin.component";
+import { ErrorComponent } from "./error/error.component";
+import { UploadComponent } from "./upload/upload.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { DownloadsComponent } from "./downloads/downloads.component";
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
@@ -10,4 +14,14 @@ export const routes: Routes = [
   { path: 'admin', component: AdminComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
+  {
+    path: 'torrent',
+    children: [
+      { path: '', redirectTo: 'upload', pathMatch: 'full' },
+      { path: 'upload', component: UploadComponent },
+      { path: 'progress/:id', component: ProgressComponent },
+      { path: 'downloads', component: DownloadsComponent },
+    ]
+  },
+  { path: '**', component: ErrorComponent },
 ];
